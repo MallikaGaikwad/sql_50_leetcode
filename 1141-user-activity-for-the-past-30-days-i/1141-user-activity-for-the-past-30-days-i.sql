768fb5cefdb0055749bb1e3953927a581dedd734
Mallika Gaikwad <158351016+MallikaGaikwad@users.noreply.github.com>
2026-03-23 13:43:53 -0400

    Time: 364 ms (50.35%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+-- may have duplicate rows, one user may have multiple session_ids and activity_types
+-- to find: daily active user count from 2019-06-27 to 2019-07-27 inclusive so have to use 07-28 while using BETWEEN clause
+-- even any one activity is conisdered active on that day
+
+select 
+    activity_date as day,
+    count(distinct user_id) as active_users
+from activity
+where activity_date BETWEEN '2019-06-28' AND '2019-07-27' 
+group by day
+having count(distinct user_id) != 0
+
+
