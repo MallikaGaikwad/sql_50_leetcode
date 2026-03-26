e054dbad332a9e4ec51bc99cffd3ed5591f61676
Mallika Gaikwad <158351016+MallikaGaikwad@users.noreply.github.com>
2026-03-25 20:43:35 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -3,6 +3,26 @@
 -- to find: all sales in the 1st year each product was sold
 -- solution: to get the first year we use row_num
 
+-- with first_yr as (
+-- select
+--     sale_id,
+--     product_id,
+--     year,
+--     quantity,
+--     price,
+--     min(year) over (partition by product_id) as min_yr
+-- from sales
+-- )
+
+-- select 
+--     product_id,
+--     min_yr as first_year,
+--     quantity,
+--     price
+-- from first_yr
+-- where min_yr= year
+
+
 with first_yr as (
 select
     sale_id,
@@ -10,14 +30,14 @@ select
     year,
     quantity,
     price,
-    min(year) over (partition by product_id) as min_yr
+    rank() over (partition by product_id order by year) as min_yr
 from sales
 )
 
 select 
     product_id,
-    min_yr as first_year,
+    year as first_year,
     quantity,
     price
 from first_yr
-where min_yr= year
+where min_yr = 1
