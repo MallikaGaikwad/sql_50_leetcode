0b889b1365067603e0afd48052e504998e2d2a65
Mallika Gaikwad <158351016+MallikaGaikwad@users.noreply.github.com>
2026-04-01 19:29:30 -0400

    Time: 306 ms (90.35%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -8,25 +8,15 @@
 -- 3. avg age of employees whoe report to them rounded to nearest integer
 -- 4. return the result order by emp id asc
 
-with emp_mgr_table as (
+
 select
-    e.employee_id,
-    e.name,
-    e.reports_to,
-    e.age,
-    m.employee_id as manager_id,
-    m.name as manager_name
+    m.employee_id,
+    m.name,
+    count(e.reports_to) as reports_count,
+    round(avg(e.age),0) as average_age
 from employees as e
 left join employees as m
 on e.reports_to = m.employee_id
 where e.reports_to is not null
-)
-
-select 
-    manager_id as employee_id,
-    manager_name as name,
-    count(reports_to) as reports_count,
-    round(avg(age),0) as average_age
-from emp_mgr_table 
-group by manager_id, manager_name
-order by employee_id
+group by m.employee_id, m.name
+order by m.employee_id asc
