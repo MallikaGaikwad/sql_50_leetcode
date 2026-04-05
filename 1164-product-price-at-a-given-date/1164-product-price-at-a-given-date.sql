c43e5f5e6c45a3219d0cb8620dcf3d8f00affc0b
Mallika Gaikwad <158351016+MallikaGaikwad@users.noreply.github.com>
2026-04-04 20:28:22 -0400

    Time: 342 ms (76.73%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -6,32 +6,30 @@
 -- logic is : 
 -- if the new price of that product exists on 08/16/2019 then that is the price
 -- if there is no record on a date on 08/16/2019 but after that date then it is default to 10,
--- if there is a price on a date before 08/16/2019 and after 08/16/2019 then the price will be the price of the date before 08/16
+-- if there is a price on a date before 08/16/2019 then the price will be the price of the date before 08/16
 -- return in any order 
 
 -- '2019-08-16'
 
--- select 
---     product_id,
---     new_price as price,
---     change_date,
---     row_number() over (partition by product_id order by change_date desc) as row_num
--- from products 
--- where change_date <= '2019-08-16'
-
-with interim as (
-    select product_id,
-    new_price as price,
-    row_number() over (partition by product_id order by change_date desc) as row_num
+select 
+    product_id,
+    new_price as price
+from products
+where (product_id, change_date) IN 
+    (select product_id, max(change_date) as change_date
     from products
     where change_date <= '2019-08-16'
-)
+    group by product_id)
 
-select product_id,
-price
-from interim
-where row_num = 1
-UNION
-select product_id, 10 as price
+
+union all
+
+select distinct
+    product_id,
+    10 as price
 from products 
-where product_id not in (select distinct product_id from interim)
+where (product_id) NOT IN 
+    (select product_id
+    from products
+    where change_date <= '2019-08-16'
+    group by product_id)
