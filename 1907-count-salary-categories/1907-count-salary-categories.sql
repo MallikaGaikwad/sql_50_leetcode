6c9a5c75c6c5c7107d27c99f5237995b4f327397
Mallika Gaikwad <158351016+MallikaGaikwad@users.noreply.github.com>
2026-04-09 19:45:17 -0400

    Time: 3177 ms (94.46%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,29 @@
+-- Write your PostgreSQL query statement below
+-- account id is the pk
+-- each row has the monthly income for one bank account
+-- to find: calculate the number of bank accounts for each salary category
+-- result must contain all categories, if no accounts then 0
+-- result in any order 
+-- logic: write a cte to categorize the salaries and create a column "category" and then group it in the main query
+
+select 
+    'Low Salary' as category,
+    count(account_id) as accounts_count
+from accounts
+where income < 20000
+
+union
+
+select 
+    'Average Salary' as category,
+    coalesce(count(account_id),0) as accounts_count
+from accounts
+where income >= 20000 and income <= 50000
+
+union
+
+select 
+    'High Salary' as category,
+    count(account_id) as accounts_count
+from accounts
+where income > 50000
