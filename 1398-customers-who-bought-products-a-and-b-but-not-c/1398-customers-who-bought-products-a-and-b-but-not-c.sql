06677e7ea4a6585a0cae59015861a8dddde1d31a
Mallika Gaikwad <158351016+MallikaGaikwad@users.noreply.github.com>
2026-05-30 22:05:36 -0400

    Time: 229 ms (54.55%), Space: 0B (100%) - LeetHub

---
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+-- customer_id - pk for customers
+-- order_id - pk for orders; customer_id - fk in orders
+
+select customer_id, customer_name
+from customers 
+where customer_id in (select customer_id from orders where product_name = 'A')
+and customer_id in (select customer_id from orders where product_name = 'B')
+and customer_id  not in (select customer_id from orders where product_name = 'C')
+order by customer_id
